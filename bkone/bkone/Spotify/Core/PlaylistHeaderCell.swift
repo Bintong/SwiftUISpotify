591c8d47f5dc6a8e70000bb889f0ded7405efb2a
tongbin <yaxun_123@163.com>
2024-04-25 20:36:02 +0800

    header

--- bkone/bkone/Spotify/Core/PlaylistHeaderCell.swift
+++ bkone/bkone/Spotify/Core/PlaylistHeaderCell.swift
@@ -10,15 +10,36 @@ import SwiftUI
  
 struct PlaylistHeaderCell: View {
     
-    var title: String = ""
-    var  subtitle: String = ""
+    var title: String = "some title of playlist here"
+    var subtitle: String = "subtitle here"
     var imageName :String = ""
+    var shadowColor: Color = .spotifyBlack.opacity(0.8)
+    
     var body: some View {
-        
-        ImageLoaderView(urlString: Constants.randomImage)
+        Rectangle()
+            .opacity(0)
+            .overlay {
+                ImageLoaderView(urlString: Constants.randomImage)
+            }
             .overlay(
-                Vstak
+                VStack(alignment: .leading, spacing: 8) {
+                    Text(subtitle)
+                        .font(.headline)
+                    Text(title)
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                }
+                    .foregroundStyle(.spotifyWhite)
+                    .padding(16.0)
+                    .frame(maxWidth: .infinity,alignment: .leading)
+                    .background(
+                        LinearGradient(colors: [shadowColor.opacity(0), shadowColor],
+                                       startPoint: .top,
+                                       endPoint: .bottom)
+                    )
+                ,alignment: .bottomLeading
             )
+            .asStretchyHeader(startingHeight: 300)
             .frame(height: 300)
         
     }
